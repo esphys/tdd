5cb1c236a4ff2a293a0101f758c0c33b9ddd1ea8
esphys <esphys@gmail.com>
2018-01-23 04:21:31 -0500

    Add Money multiplication Test

---
+++ src/test/java/org/esphys/ttd/MoneyTest.java
@@ -0,0 +1,15 @@
+package org.esphys.ttd;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by Evan_2 on 2018-01-23.
+ */
+public class MoneyTest extends TestCase{
+    public void testMultiplication() throws Exception {
+        Dollar five = new Dollar(5);
+        five.times(2);
+        assertEquals(10, five.ammount);
+
+    }
+}
