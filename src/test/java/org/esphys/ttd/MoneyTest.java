d953007822bc581fbb8a55f593ed89717358d5ac
esphys <esphys@gmail.com>
2018-01-23 04:22:26 -0500

    fix spelling mistake

--- src/test/java/org/esphys/ttd/MoneyTest.java
+++ src/test/java/org/esphys/ttd/MoneyTest.java
@@ -9,7 +9,7 @@ public class MoneyTest extends TestCase{
     public void testMultiplication() throws Exception {
         Dollar five = new Dollar(5);
         five.times(2);
-        assertEquals(10, five.ammount);
+        assertEquals(10, five.amount);
 
     }
 }
