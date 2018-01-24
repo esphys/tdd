d3f9f4e07a7766801002fff8b10ae629eacba40b
esphys <esphys@gmail.com>
2018-01-24 03:41:37 -0500

    add equals() method to Dollar and test method

--- src/test/java/org/esphys/ttd/MoneyTest.java
+++ src/test/java/org/esphys/ttd/MoneyTest.java
@@ -6,11 +6,14 @@ import junit.framework.TestCase;
  * Created by Evan_2 on 2018-01-23.
  */
 public class MoneyTest extends TestCase{
-    public void testMultiplication() throws Exception {
+    public void testMultiplication(){
         Dollar five = new Dollar(5);
         Dollar product = five.times(2);
         assertEquals(10, product.amount);
         product = five.times(3);
         assertEquals(15, product.amount);
+    }
+    public void testEquality(){
+        assertTrue(new Dollar(5).equals(new Dollar(5)));
     }
 }
